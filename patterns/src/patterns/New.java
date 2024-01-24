9fb933bb1d545e0f0ff85cc7e507966f06994545
Sushanthgowda1 <157610813+Sushanthgowda1@users.noreply.github.com>
2024-01-24 19:42:24 +0530

    Add files via upload

---
+++ patterns/src/patterns/New.java
@@ -0,0 +1,58 @@
+package patterns;
+
+public class New {
+
+	public static void main(String[] args) {
+		
+		int n= 5;
+		
+		for(int i= 1; i<= n ;i++) {
+			
+			
+			for (int j= 1 ; j<=i; j++) {
+				
+				System.out.print(j);
+			}
+	
+			for(int j= 0; j<= 2*(n-i)-1 ; j++) {
+				
+				System.out.print("0");
+			}
+			
+			
+			for(int j=i; j>=1 ;j--) {
+				
+				System.out.print(j);
+			}
+			
+			System.out.println();
+			
+		}
+		
+		for(int i= n; i>=1 ;i--) {
+			
+			
+			for (int j= 1 ; j<=i; j++) {
+				
+				System.out.print(j);
+			}
+	
+			for(int j= 0; j<= 2*(n-i)-1 ; j++) {
+				
+				System.out.print("0");
+			}
+			
+			
+			for(int j=i; j>=1 ;j--) {
+				
+				System.out.print(j);
+			}
+			
+			System.out.println();
+			
+		}
+		
+
+	}
+
+}
---
+++ patterns/src/patterns/Pattern.java
@@ -0,0 +1,31 @@
+package patterns;
+
+public class Pattern {
+	
+	public static void main (String[] args) {
+	
+	int n=5;
+	
+	for(int i= 1; i<=n ; i++) {
+		//spaces
+		
+		for (int j= 1; j<= n-i ; j++) {
+			
+			System.out.print(" ");
+				
+		}
+		
+		for (int j= 1; j<=i ; j++) {
+			
+			System.out.print(i+" ");
+		}
+		
+		for(int j= n; j>= n-i; j--) {
+			
+			System.out.print(i);
+		}
+		
+		System.out.println();
+	}
+}
+}
